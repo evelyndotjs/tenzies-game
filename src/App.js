7ae63c2fba18d6c5fa36eda84036b9f73914dc5c
Evelyn Huang <evelyn@Evelyns-MacBook-Pro.local>
2022-03-17 23:58:56 +0100

    Update state array elements to objects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import { useState } from "react";
 import "./style.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
   const [dice, setDice] = useState(allNewDice());
 
   function allNewDice() {
     const newDice = [];
-
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: true,
+        id: nanoid(),
+      });
     }
     return newDice;
   }
 
-  const dieValue = dice.map((die) => <Die value={die} />);
-
-  function handleClick() {
+  function rollDice() {
     setDice(allNewDice());
   }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+  ));
 
   return (
     <main>
@@ -27,8 +33,8 @@ export default function App() {
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
-      <div className="dice--container">{dieValue}</div>
-      <button className="btn" onClick={handleClick}>
+      <div className="dice--container">{diceElements}</div>
+      <button className="btn" onClick={rollDice}>
         Roll
       </button>
     </main>
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,9 @@
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#FFF",
+  };
   return (
-    <div className="die--face">
+    <div className="die--face" style={styles}>
       <h2 className="die--boxes">{props.value}</h2>
     </div>
   );
